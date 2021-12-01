b69a9eaab89648fef7954af95a692de6af8fc1cd
John Doe <johndoe@example.com>
2021-12-01 21:20:02 +0100

    day2 tests

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {    
+    #[test]
+    fn test_first() {
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_second() {
+        assert_eq!(1, 1);
+    }
+}
